cc4003eb7866e10c5a17ddb093d2e452270f8d0e
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-02-13 21:41:41 -0300

    Add files via upload

--- js/Form.js
+++ js/Form.js
@@ -12,4 +12,23 @@ class Form {
     this.input.hide();
   }
 
+  setElementsPosition() {
+    this.titleImg.position(150,50);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  display() {
+    this.setElementsPosition();
+    this.setElementsStyle();
+  }
+
 }
