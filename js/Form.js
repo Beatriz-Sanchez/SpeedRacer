aea9301f8ef66873d8c24e9c9acdae39565d7964
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-02-17 10:24:26 -0300

    Add files via upload

--- js/Form.js
+++ js/Form.js
@@ -1,19 +1,13 @@
 class Form {
   constructor() {
-    this.input = createInput("").attribute("placeholder", "Digite seu nome");
+    this.input = createInput("").attribute("placeholder", "Digite Seu Nome");
     this.playButton = createButton("Jogar");
-    this.titleImg = createImg("./assets/TITULO.png", "nome do jogo");
+    this.titleImg = createImg("./assets/TITULO.png", "game title");
     this.greeting = createElement("h2");
   }
 
-  hide() {
-    this.greeting.hide();
-    this.playButton.hide();
-    this.input.hide();
-  }
-
   setElementsPosition() {
-    this.titleImg.position(150,50);
+    this.titleImg.position(120, 50);
     this.input.position(width / 2 - 110, height / 2 - 80);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
@@ -26,12 +20,18 @@ class Form {
     this.greeting.class("greeting");
   }
 
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
   handleMousePressed() {
     this.playButton.mousePressed(() => {
       this.input.hide();
       this.playButton.hide();
       var message = `
-      Olá, ${this.input.value()}
+      Olá ${this.input.value()}
       </br>espere o outro jogador entrar...`;
       this.greeting.html(message);
     });
@@ -42,5 +42,4 @@ class Form {
     this.setElementsStyle();
     this.handleMousePressed();
   }
-
 }
